d57e2d1a1df16b43a3bce833688392ccfc23fe67
RoozbehZhuleh <zhuleh.developer@gmail.com>
2022-08-03 22:09:26 +0430

    added swiper JS

--- swiper.js
+++ swiper.js
@@ -1,28 +1,19 @@
- // import Swiper bundle with all modules installed
-  import Swiper from 'swiper/bundle';
+//  // import Swiper bundle with all modules installed
+//   import Swiper from 'swiper/bundle';
 
-  // import styles bundle
-  import 'swiper/css/bundle';
+//   // import styles bundle
+//   import 'swiper/css/bundle';
 
   // init Swiper:
   const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
+  direction: "horizontal",
+  slidesPerView: 4,
+    spaceBetween: 0,
+navigation: {
+nextEl: ".swiper-button-next",
+prevEl: ".swiper-button-prev",
+},
+  });
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});

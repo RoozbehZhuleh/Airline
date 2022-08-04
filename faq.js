eab7178ce75bd4c7f4128c8f566b7587961ad5ad
RoozbehZhuleh <zhuleh.developer@gmail.com>
2022-08-04 17:48:49 +0430

    updated faq

--- faq.js
+++ faq.js
@@ -3,15 +3,25 @@ const accText = document.querySelectorAll('.accordion__item__text');
 const accArrow = document.querySelectorAll('.accordion__arrow');
 const accItem = document.querySelectorAll('.accordion__item');
 
-accBtn.forEach((btn,i) => {
-    btn.addEventListener('click', () => {
-        accBtn.forEach(btn => {
-            accItem.classList.remove("accordion--active");
-            // console.log(btn);
-        });
+// accBtn.forEach((btn,i) => {
+//     btn.addEventListener('click', () => {
+//         accBtn.forEach(btn => {
+//             accItem.classList.remove("accordion--active");
+//             // console.log(btn);
+//         });
         
-        accItem[i].classList.add("accordion--active");
+//         accItem[i].classList.add("accordion--active");
 
+//     })
+// })
+
+accItem.forEach((e) => {
+    // console.log(e);
+    e.addEventListener('click', (event) => {
+        accItem.forEach((i) => {
+            i.classList.remove("accordion--active");
+        })
+        e.classList.add("accordion--active");
     })
 })
 
